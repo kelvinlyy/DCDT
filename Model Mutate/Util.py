abd3257c6a9a85b05004d33cb2f79f24fe5ccba7
Yukititit <cyuk1115@gmail.com>
2022-03-28 23:31:30 +0800

    tidy up

--- Model Mutate/Util.py
+++ Model Mutate/Util.py
@@ -2,6 +2,8 @@ import coverage
 from tensorflow import keras
 import pandas as pd
 
+from Layer_Util import addActivation, addAverPool1D, addAverPool2D, addAverPool3D, addBatchNorm, addConv1D, addConv2D, addConv2DTranspose, addConv3D, addConv3DTranspose, addDense, addDepthwiseConv2D, addDropout, addFlatten, addGRU, addGRUCell, addGaussianDropout, addGaussianNoise, addGloAverPool1D, addGloAverPool2D, addGloAverPool3D, addGloMaxPool1D, addGloMaxPool2D, addGloMaxPool3D, addLSTM, addLSTMCell, addLayerNormal, addLeakyReLU, addLocallyConnected1D, addLocallyConnected2D, addMaxPool, addReLU, addRepeatVector, addReshape, addSeparableConv1D, addSeparableConv2D, addSimpleRNN, addSimpleRNNCell, addSpatialDropout1D, addSpatialDropout2D, addSpatialDropout3D, addThresholdedReLU, addZeroPadd, buildModelByArtchitect, extractModelArchitect, get_paras_num, modelReduction
+
 def lines2Vector(lines):
     lineVector = []
     if lines:
@@ -43,81 +45,3 @@ def delta_lines(covList1, covList2):
         cnt += len(b.difference(a))    
         
     return cnt
-
-# class Class_Function:    
-    
-#     def __init__(self, name, lines):
-#         self.name = name
-#         self.lines = lines
-#         self.length = len(lines)
-#         self.coverage = self.calc_coverage()
-        
-#     def calc_coverage(self):
-#         if (self.length == 0):
-#             return 0
-#         else:
-#             c = 0
-#             for line in self.lines:
-#                 if (line[1] == '1'):
-#                     c += 1
-#             return c/self.length
-
-# class Package:
-    
-#     def __init__(self, name):
-#         self.name = name
-#         self.classes = []
-#         self.coverage = 0
-    
-#     def calc_coverage(self):
-#         tmp = 0
-#         count = 0
-#         for _ in self.classes:
-#             tmp += _.coverage
-#             count += 1
-#         return tmp/count
-
-# class Coverage_Table: 
-    
-#     def __init__(self):
-#         self.packages = []
-#         self.coverage = 0
-        
-#     def calc_coverage(self):
-#         tmp = 0
-#         count = 0
-#         for _ in self.packages:
-#             tmp += _.coverage
-#             count += 1
-#         return tmp/count
-        
-#     def print_details(self):
-#         for i in self.packages:
-#             print(i.name)
-#             for j in i.classes:
-#                 print(f'\t{j.name}')
-#                 for k in j.lines:
-#                     print(f'\t\t{k}')
-        
-# def init_CovTable():
-    
-#     import xml.etree.ElementTree as ET
-#     tree = ET.parse('coverage.xml')
-#     packages = tree.getroot()[1]
-
-#     cov_table = Coverage_Table()
-#     for package in packages:
-#         new_package = Package(package.get('name'))
-#         for class_function in package[0]:
-#             line_num = []
-#             line_hit = []
-#             for line in class_function[1]:
-#                 line_num.append(line.get('number'))
-#                 line_hit.append(line.get('hits'))
-#             new_class = Class_Function(class_function.get('filename'), pd.DataFrame([line_num, line_hit]).T.values.tolist())
-#             new_package.classes.append(new_class)
-#         new_package.coverage = new_package.calc_coverage()
-#         cov_table.packages.append(new_package)
-#     cov_table.coverage = cov_table.calc_coverage()
-    
-#     return cov_table
