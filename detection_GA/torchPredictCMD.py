f5951a9c35c67b388de9d7b899fd6405dcff993e
kelvin <kelvinklyy@gmail.com>
2022-04-25 01:06:45 +0800

    Update

---
+++ detection_GA/torchPredictCMD.py
@@ -0,0 +1,17 @@
+py_env = '/data/yylaiai/anaconda3/envs/fyp_v3/bin/python'
+predictUtil_path = "predictUtil"
+
+get_prediction_py = f"{predictUtil_path}/get_prediction_torch.py"
+
+'''
+Arguments:
+[1]: db flag of redis server
+[2]: model key
+[3]: input key
+[4]: layer idx
+'''
+
+# generate command for computing model prediction (only last layer)
+def get_prediction_cmd(db_flag, model_key, input_key, layer_idx):
+    cmd = f"{py_env} {get_prediction_py} {db_flag} {model_key} {input_key} {layer_idx}"
+    return cmd
